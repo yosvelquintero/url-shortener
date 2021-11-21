71cd6b775f8a3422a6e58216a8332462dc4d3ad3
Yosvel Quintero <yosvelquintero@gmail.com>
2021-11-21 14:12:01 +0700

    Environment code refactor

--- src/config/constants/env.ts
+++ src/config/constants/env.ts
@@ -2,14 +2,15 @@ import { DeepReadonly } from 'ts-essentials';
 import { IEnv } from '@url-shortener/types/index';
 
 export const ENV: DeepReadonly<IEnv> = {
-  database: {
-    app: {
-      api: {
-        host: 'APP_API_HOST',
-        port: 'APP_API_PORT',
-        prefix: 'APP_API_PREFIX',
-      },
+  app: {
+    api: {
+      host: 'APP_API_HOST',
+      port: 'APP_API_PORT',
+      prefix: 'APP_API_PREFIX',
+      isProduction: 'APP_API_IS_PRODUCTION',
     },
+  },
+  database: {
     mongodb: {
       mongodbHost: 'MONGODB_HOST',
       mongodbDb: 'MONGODB_DB',
