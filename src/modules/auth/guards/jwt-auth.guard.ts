eb6f80e9cff6a875e780d42f9a7a1b4f5641aefd
Yosvel Quintero <yosvelquintero@gmail.com>
2021-11-27 17:34:25 +0700

    Code refactor

--- src/modules/auth/guards/jwt-auth.guard.ts
+++ src/modules/auth/guards/jwt-auth.guard.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@nestjs/common';
 import { AuthGuard } from '@nestjs/passport';
 
-import { AUTH } from '@app/config/index';
+import { AUTH } from '@app/config';
 
 @Injectable()
 export class JwtAuthGuard extends AuthGuard(AUTH.guards.jwt) {}
